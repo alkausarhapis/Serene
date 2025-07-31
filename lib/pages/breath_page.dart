50d64437d925e96fdc0f4d752848091756182280
alkausarhapis <alkausarhapis@gmail.com>
2025-07-31 21:15:33 +0700

    init: setup new pages

---
+++ lib/pages/breath_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BreatheExercise extends StatelessWidget {
+  const BreatheExercise({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Breathe")),
+    );
+  }
+}
---
+++ lib/pages/pomodoro_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PomodoroPage extends StatelessWidget {
+  const PomodoroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text("Pomodoro"),
+    ));
+  }
+}
